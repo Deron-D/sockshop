ce935fef22612b770284cc304d31c794d77e6da7
Dmitriy Pnev <dmitriypnev@gmail.com>
2023-04-08 22:05:05 +0300

    Submodules remove

--- submodules_add.sh
+++ submodules_add.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#git rm -f src/carts
-#git rm -f src/front-end
-#git rm -f src/user
-#git rm -f src/shipping
-#git rm -f src/queue-master
-#git rm -f src/msd-java
-#git rm -f src/catalogue
-#git rm -f src/orders
-#git commit -am "Submodules remove"
+git rm -f src/carts
+git rm -f src/front-end
+git rm -f src/user
+git rm -f src/shipping
+git rm -f src/queue-master
+git rm -f src/msd-java
+git rm -f src/catalogue
+git rm -f src/orders
+git commit -am "Submodules remove"
 #git submodule add -f git@github.com:Deron-D/carts.git src/carts
 #git submodule add -f git@github.com:Deron-D/front-end.git src/front-end
 #git submodule add -f git@github.com:Deron-D/user.git src/user
