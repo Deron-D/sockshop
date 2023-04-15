898f374b1f0847bb4e068d22eb39d67c7afb8bad
Dmitriy Pnev <dmitriypnev@gmail.com>
2023-04-15 21:55:31 +0300

    Submodules remove

--- submodules_add.sh
+++ submodules_add.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-git rm -f src/carts
-git rm -f src/front-end
-git rm -f src/user
-git rm -f src/shipping
-git rm -f src/queue-master
-git rm -f src/msd-java
-git rm -f src/catalogue
-git rm -f src/orders
+git rm -f carts
+git rm -f front-end
+git rm -f user
+git rm -f shipping
+git rm -f queue-master
+git rm -f msd-java
+git rm -f catalogue
+git rm -f orders
 git commit -am "Submodules remove"
-git submodule add -f git@github.com:microservices-demo/carts.git carts
-git submodule add -f git@github.com:microservices-demo/front-end.git front-end
-git submodule add -f git@github.com:microservices-demo/user.git user
-git submodule add -f git@github.com:microservices-demo/shipping.git shipping
-git submodule add -f git@github.com:microservices-demo/queue-master.git queue-master
-git submodule add -f git@github.com:microservices-demo/msd-java.git msd-java
-git submodule add -f git@github.com:microservices-demo/catalogue.git catalogue
-git submodule add -f git@github.com:microservices-demo/orders.git orders
-
-
+#git submodule add -f git@github.com:microservices-demo/carts.git carts
+#git submodule add -f git@github.com:microservices-demo/front-end.git front-end
+#git submodule add -f git@github.com:microservices-demo/user.git user
+#git submodule add -f git@github.com:microservices-demo/shipping.git shipping
+#git submodule add -f git@github.com:microservices-demo/queue-master.git queue-master
+#git submodule add -f git@github.com:microservices-demo/msd-java.git msd-java
+#git submodule add -f git@github.com:microservices-demo/catalogue.git catalogue
+#git submodule add -f git@github.com:microservices-demo/orders.git orders
+#
+#
