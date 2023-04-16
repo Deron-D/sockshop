5c7fce47a82b86eeab4f8ac29f7c2b5955660430
Dmitriy Pnev <dmitriypnev@gmail.com>
2023-04-16 10:41:22 +0300

    Submodules remove

--- submodules_add.sh
+++ submodules_add.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#git rm -f carts
-#git rm -f front-end
-#git rm -f user
-#git rm -f shipping
-#git rm -f queue-master
-#git rm -f msd-java
-#git rm -f catalogue
-#git rm -f orders
-#git commit -am "Submodules remove"
-#git submodule add -f git@github.com:microservices-demo/carts.git carts
-git submodule add -f git@github.com:microservices-demo/front-end.git front-end
-git submodule add -f git@github.com:microservices-demo/user.git user
-git submodule add -f git@github.com:microservices-demo/shipping.git shipping
-git submodule add -f git@github.com:microservices-demo/queue-master.git queue-master
-git submodule add -f git@github.com:microservices-demo/msd-java.git msd-java
-git submodule add -f git@github.com:microservices-demo/catalogue.git catalogue
-git submodule add -f git@github.com:microservices-demo/orders.git orders
+git rm -f carts
+git rm -f front-end
+git rm -f user
+git rm -f shipping
+git rm -f queue-master
+git rm -f msd-java
+git rm -f catalogue
+git rm -f orders
+git commit -am "Submodules remove"
+git submodule add -f git@github.com:Deron-D/carts.git carts
+git submodule add -f git@github.com:Deron-D/front-end.git front-end
+git submodule add -f git@github.com:Deron-D/user.git user
+git submodule add -f git@github.com:Deron-D/shipping.git shipping
+git submodule add -f git@github.com:Deron-D/queue-master.git queue-master
+git submodule add -f git@github.com:Deron-D/msd-java.git msd-java
+git submodule add -f git@github.com:Deron-D/catalogue.git catalogue
+git submodule add -f git@github.com:Deron-D/orders.git orders
 #
 #
