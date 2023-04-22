dc9ff6bdda41a343a5937eac83c9356286340506
Dmitriy Pnev <dmitriypnev@gmail.com>
2023-04-22 17:40:23 +0300

    Submodules remove

--- submodules_add.sh
+++ submodules_add.sh
@@ -9,15 +9,14 @@ git rm -f catalogue
 git rm -f orders
 git rm -f payment
 git commit -am "Submodules remove"
-#git submodule add -f git@github.com:Deron-D/carts.git carts
-#git submodule add -f git@github.com:Deron-D/front-end.git front-end
-#git submodule add -f git@github.com:Deron-D/user.git user
-#git submodule add -f git@github.com:Deron-D/shipping.git shipping
-#git submodule add -f git@github.com:Deron-D/queue-master.git queue-master
-#git submodule add -f git@github.com:Deron-D/msd-java.git msd-java
-#git submodule add -f git@github.com:Deron-D/catalogue.git catalogue
-#git submodule add -f git@github.com:Deron-D/orders.git orders
-#git submodule add -f git@github.com:Deron-D/payment.git payment
+git submodule add -f https://github.com/Deron-D/carts.git carts
+git submodule add -f https://github.com/Deron-D/front-end.git front-end
+git submodule add -f https://github.com/Deron-D/user.git user
+git submodule add -f https://github.com/Deron-D/shipping.git shipping
+git submodule add -f https://github.com/Deron-D/queue-master.git queue-master
+git submodule add -f https://github.com/Deron-D/msd-java.git msd-java
+git submodule add -f https://github.com/Deron-D/catalogue.git catalogue
+git submodule add -f https://github.com/Deron-D/orders.git orders
+git submodule add -f https://github.com/Deron-D/payment.git payment
 
-#
-#
+
